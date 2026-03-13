45806f2422b6b8a9c9d29c3f492fde2ce95138b2
Prahalad kumar <prahaladkr1@gmail.com>
2026-03-13 14:33:00 +0530

    Enhance logger configuration with file handler
    Added file handler to logger for logging to a file.

--- Bot/logger_config.py
+++ Bot/logger_config.py
@@ -3,9 +3,15 @@ import logging
 logger = logging.getLogger("email_bot")
 logger.setLevel(logging.INFO)
 
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
 )
 
+file_handler = logging.FileHandler("email_bot.log")
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 logger.addHandler(console_handler)
