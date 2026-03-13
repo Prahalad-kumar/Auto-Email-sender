2ea20339d0d57a92cc2a816005a04a9fb6f2747f
Prahalad kumar <prahaladkr1@gmail.com>
2026-03-13 05:12:49 +0000

    remove log file cand config logger

--- Bot/logger_config.py
+++ Bot/logger_config.py
@@ -3,15 +3,9 @@ import logging
 logger = logging.getLogger("email_bot")
 logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter(
-    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 )
 
-file_handler = logging.FileHandler("email_bot.log")
-file_handler.setFormatter(formatter)
-
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
 logger.addHandler(console_handler)
